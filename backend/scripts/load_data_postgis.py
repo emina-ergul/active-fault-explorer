4c745af493183c7e411e82bd89f880eaf4a228b8
emina <eminaergul712@gmail.com>
2026-05-02 20:16:16 +0100

    added lambda for removing z from fault geometries

--- backend/scripts/load_data_postgis.py
+++ backend/scripts/load_data_postgis.py
@@ -1,5 +1,6 @@
-from database.init_db import engine
+from backend.database.init_db import engine
 import geopandas as gpd
+import shapely
 
 
 def load_earthquakes():
@@ -11,6 +12,10 @@ def load_earthquakes():
 def load_faults():
     gdf = gpd.read_file("backend/data/raw/faults/faults.geojson")
     gdf = gdf.to_crs(4326)
+    print(gdf.geometry.has_z.sum())
+    gdf["geometry"] = gdf["geometry"].apply(
+        lambda g: shapely.force_2d(g) if g is not None else None
+    )
     gdf.to_postgis(name="faults", con=engine, if_exists="append")
 
 

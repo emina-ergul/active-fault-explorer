5e8fa3fbd1dfba462d1e52b921ed8e3584745cdb
emina <eminaergul712@gmail.com>
2026-05-09 11:10:13 +0100

    corrected query

---
+++ backend/queries/queries.py
@@ -0,0 +1,26 @@
+from sqlalchemy import text
+from backend.database.init_db import engine
+
+
+def get_distance(quake_id: str):
+    query = text("""
+        SELECT
+            f.name,
+            e.id,
+            e.magnitude,
+            ST_Distance(
+            e.geometry::geography,
+            f.geometry::geography
+        ) AS distance_m
+        FROM earthquakes e, faults f
+        WHERE e.id = :quake_id
+        ORDER BY distance_m ASC
+        LIMIT 1
+    """)
+
+    with engine.connect() as conn:
+        res = conn.execute(query, {"quake_id": quake_id}).fetchone()
+        print("$$$$$$$$$$$", res)
+
+
+get_distance("us7000rlkk")

ed6a7f1563cf93415e7f1e959f391b2547f7b52a
emina <eminaergul712@gmail.com>
2026-05-09 15:42:02 +0100

    query progress

--- backend/queries/queries.py
+++ backend/queries/queries.py
@@ -1,12 +1,58 @@
+import datetime
+from datetime import timezone
 from sqlalchemy import text
 from backend.database.init_db import engine
+
+now = datetime.datetime.now(timezone.utc)
+
+
+def get_earthqauke_info(quake_id: str):
+    query = text("""
+    SELECT * 
+    FROM earthquakes
+    WHERE id = :quake_id 
+    """)
+
+    with engine.connect() as conn:
+        res = conn.execute(query, {"quake_id": quake_id}).fetchall()
+        print(res)
+
+
+def get_fault_info(fault_id: str):
+    query = text("""
+    SELECT
+        catalog_id,
+        name,
+        last_movement,
+        slip_type,
+        net_slip_rate,
+        notes,
+        ST_Union(geometry) AS geometry
+    FROM faults
+    WHERE catalog_id = :fault_id
+    GROUP BY
+        catalog_id,
+        name,
+        last_movement,
+        slip_type,
+        net_slip_rate,
+        notes
+""")
+
+    with engine.connect() as conn:
+        res = conn.execute(query, {"fault_id": fault_id}).fetchall()
+        print("$$$$$$$$$$$", res)
 
 
 def get_distance(quake_id: str):
     query = text("""
         SELECT
-            f.name,
             e.id,
+            f.name,
+            f.catalog_id,
+            f.last_movement,
+            f.slip_type,
+                 f.net_slip_rate,
             e.magnitude,
             ST_Distance(
             e.geometry::geography,
@@ -21,7 +67,7 @@ def get_distance(quake_id: str):
         )
         WHERE e.id = :quake_id
         ORDER BY distance_m ASC
-        LIMIT 1
+        LIMIT 10
     """)
 
     with engine.connect() as conn:
@@ -30,3 +76,5 @@ def get_distance(quake_id: str):
 
 
 get_distance("us7000rlkk")
+get_earthqauke_info("us7000rlkk")
+get_fault_info("SA_410")
